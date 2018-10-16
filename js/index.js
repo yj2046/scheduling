da6fa4d46a210b446f85998d5edaeeb933d61a20
yangjing <1036808695@qq.com>
2018-10-16 10:41:53 +0800

    drag

--- js/index.js
+++ js/index.js
@@ -41,6 +41,13 @@ Vue.directive("longtouch", function(el, binding) {
 var app = new Vue({
     el:"#app",
     data:{
+        dayList:[
+            {name:"11111",text:"aaaaa"},
+            {name:"22222",text:"bbbbb"},
+            {name:"33333",text:"ccccc"},
+            {name:"44444",text:"ddddd"},
+            {name:"55555",text:"eeeee"},
+        ],
         playList:[
             {name:"赏花",status:0},
             {name:"骑行",status:0},
@@ -99,6 +106,69 @@ var app = new Vue({
             if (_this.num > 1) {
                data.status = 2;
             }
+        },
+        onEnd:function (day,index) {
+
+            var indexNew = this.numberText(index);
+        },
+        /**
+         * 阿拉伯数字转汉字
+         * @param numberText
+         * @returns {*}
+         */
+        translateNumber: function (numberText) {
+            var CHINESE_NEGATIVE = "负";
+            var CHINESE_ZERO = "零";
+            var CHINESE_DIGITS = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+            var CHINESE_UNITS = ["", "十", "百", "千"];
+            var CHINESE_GROUP_UNITS = ["", "万", "亿", "兆", "京", "垓", "杼", "穰", "溝", "澗", "正", "載", "極"];
+            if (numberText === "") {
+                return "";
+            }
+            numberText = numberText.toString().replace(/^0+/g, "");
+            numberText = numberText.toString().replace(/^-0+/g, "-");
+            if (numberText === "" || numberText === "-") {
+                return CHINESE_ZERO;
+            }
+            var result = "";
+            if (numberText[0] === "-") {
+                result += CHINESE_NEGATIVE;
+                numberText = numberText.substring(1);
+            }
+
+            var groupIsZero = true;
+            var needZero = false;
+            for (var i = 0; i < numberText.length; ++i) {
+                var position = numberText.length - 1 - i;
+                var digit = parseInt(numberText[i]);
+                var unit = position % CHINESE_UNITS.length;
+                var group = (position - unit) / CHINESE_UNITS.length;
+
+                if (digit !== 0) {
+                    if (needZero) {
+                        result += CHINESE_ZERO;
+                    }
+
+                    if (digit !== 1 || unit !== 1 || !groupIsZero || (group === 0 && needZero)) {
+                        result += CHINESE_DIGITS[digit];
+                    }
+
+                    result += CHINESE_UNITS[unit];
+                }
+
+                groupIsZero = groupIsZero && (digit === 0);
+
+                if (unit === 0 && !groupIsZero) {
+                    result += CHINESE_GROUP_UNITS[group];
+                }
+
+                needZero = (digit === 0 && (unit !== 0 || groupIsZero));
+
+                if (unit === 0) {
+                    groupIsZero = true;
+                }
+            }
+            return result;
         }
 
     }
